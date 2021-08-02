190630015109b0642b368823c9f3ffda8d9e4e0f
zownbo <zownbo@cn.ibm.com>
2021-08-02 23:04:51 +0800

    quick sort upgrade upload

--- src/dataStructure/sort/QuickSort.java
+++ src/dataStructure/sort/QuickSort.java
@@ -3,6 +3,11 @@ package dataStructure.sort;
 import java.util.Arrays;
 
 public class QuickSort {
+    /*
+    快速排序是对冒泡排序的一种改进。基本思想是:通过一趟排序将要排序的数据分割成独立的两部分,
+    其中一部分的所有数据都比另一部分的所有数据都要小,然后再按此方法对这两个部分分别进行快速排序,
+    整个排序过程可以递归进行,以此达到整个数据变成有序序列。
+     */
     public static void main(String[] args) {
         int[] arr = {-9, 78, 0, 23, -567, 70};
         int[] arr2 = {-9, 78, 0, 23, -567, 70, -1, 900, 4578};
