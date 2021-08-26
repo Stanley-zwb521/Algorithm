4288499b7734add27e9d461475ad0ee3039930de
zownbo <zownbo@cn.ibm.com>
2021-08-26 18:01:15 +0800

    sequential storage of binary tree upload

--- src/dataStructure/tree/SequentialStorageOfBinaryTree.java
+++ src/dataStructure/tree/SequentialStorageOfBinaryTree.java
@@ -17,7 +17,7 @@ public class SequentialStorageOfBinaryTree {
      */
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7};
-        ArrayBinaryTree arrayBinaryTree=new ArrayBinaryTree(arr);
+        ArrayBinaryTree arrayBinaryTree = new ArrayBinaryTree(arr);
         arrayBinaryTree.preOrder();//1245367
     }
 }
@@ -35,25 +35,26 @@ class ArrayBinaryTree {
     /**
      * 重载preOrder方法,方便调用
      */
-    public void preOrder(){
+    public void preOrder() {
         this.preOrder(0);
     }
 
     /**
      * 编写一个方法,完成顺序存储二叉树的前序遍历
+     *
      * @param index 数组的下标
      */
-    public void preOrder(int index){
+    public void preOrder(int index) {
         //如果数组为空,或者arr.length==0
-        if(arr==null||arr.length==0){
+        if (arr == null || arr.length == 0) {
             System.out.println("数组为空,不能按照二叉树的前序遍历");
         }
         System.out.println(arr[index]);
-        if(2*index+1<arr.length){
-            preOrder(2*index+1);
+        if (2 * index + 1 < arr.length) {
+            preOrder(2 * index + 1);
         }
-        if(2*index+2<arr.length){
-            preOrder(2*index+2);
+        if (2 * index + 2 < arr.length) {
+            preOrder(2 * index + 2);
         }
     }
 }
