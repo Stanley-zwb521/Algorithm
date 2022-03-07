05b7fc14d661c64217e7a982b13e57076995042f
Stanley <zhaowenbo970521@outlook.com>
2022-03-08 00:10:00 +0800

    depth first search algorithm of map upload

--- src/dataStructure/map/Map.java
+++ src/dataStructure/map/Map.java
@@ -7,9 +7,25 @@ public class Map {
     //图是一种数据结构,其中节点可以具有零个或者多个相邻元素。两个节点之前的连接称为边。节点也可以成为顶点
     //有两种方式表示图:邻接矩阵(二维数组)和邻接表(数组+链表)
     //邻接矩阵是表示图形中顶点之间的相邻关系的矩阵
+    /*
+    图的深度优先搜索(depth first search)
+    深度优先遍历:从初始节点出发,初始访问节点可能有多个邻接节点,深度优先遍历的策略就是首先访问第一个邻接节点,
+    然后再以这个被访问的邻接节点作为初始节点,访问它的第一个邻接节点。可以这样理解:每次都在访问完当前节点后
+    首先访问当前节点的第一个邻接节点。
+     */
+    /*
+    深度优先遍历算法步骤:
+    1.访问初始节点v,并标记节点v为已访问
+    2.查找节点v的第一个邻接节点w
+    3.若w存在则继续执行4,如果w不存在则回到第1步,将从v的下一个节点继续
+    4.若w未被访问,对w进行深度优先遍历递归(即把w当作另一个v,然后进行步骤123)
+    5.若w被访问过,查找节点v的w邻接节点的下一个邻接节点,转到步骤3
+     */
     private ArrayList<String> vertexList;//存储顶点集合
     private int[][] edges;//存储图对应的邻接矩阵
     private int numOfEdges;//表示边的数目
+    //定义一个数组boolean[],记录某个节点是否被访问
+    private boolean[] isVisited;
 
     public static void main(String[] args) {
         //测试图的创建
@@ -29,6 +45,9 @@ public class Map {
         map.insertEdge(1, 4, 1);//B-E
         //显示邻接矩阵
         map.showMap();
+        //测试深度优先遍历算法
+        System.out.println("深度优先遍历算法:");
+        map.depthFirstSearch();//A->B->C->D->E
     }
 
     //构造器
@@ -37,10 +56,64 @@ public class Map {
         edges = new int[n][n];
         vertexList = new ArrayList<String>(n);
         numOfEdges = 0;
+        isVisited = new boolean[n];
+    }
+
+    /**
+     * 得到第一个邻接节点的下标 w
+     *
+     * @param index 当前节点的下标
+     * @return 如果存在就返回对应下标, 否则返回-1
+     */
+    public int getFirstNeighbor(int index) {
+        for (int j = 0; j < vertexList.size(); j++) {
+            if (edges[index][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    //根据前一个邻接节点的下标来获取下一个邻接节点
+    public int getNextNeighbor(int v1, int v2) {
+        for (int j = v2 + 1; j < vertexList.size(); j++) {
+            if (edges[v1][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    //深度优先遍历算法
+    //i 第一次就是0
+    public void depthFirstSearch(boolean[] isVisited, int i) {
+        //首先访问该节点并输出
+        System.out.printf(getValueByIndex(i) + "->");
+        //将该节点设置为已访问
+        isVisited[i] = true;
+        //查找节点v的第一个邻接节点w
+        int w = getFirstNeighbor(i);
+        while (w != -1) {//说明有邻接节点
+            if (!isVisited[w]) {
+                depthFirstSearch(isVisited, w);
+            }
+            //如果w节点已经被访问过,查找节点v的w邻接节点的下一个邻接节点
+            w = getNextNeighbor(i, w);
+        }
+    }
+
+    //对depthFirstSearch进行一个重载,遍历所有的节点,并进行dfs
+    private void depthFirstSearch() {
+        //遍历所有的节点进行dfs[回溯]
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if (!isVisited[i]) {
+                depthFirstSearch(isVisited, i);
+            }
+        }
     }
 
     //返回顶点的个数
-    public int getNumOfEdge() {
+    public int getNumOfVertex() {
         return vertexList.size();
     }
 
